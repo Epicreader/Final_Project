5181c9c82edc0b85d45ecd997ae6c24b8b3a0b1d
Trent <tpeterson998@west-mec.org>
2019-12-04 15:01:23 -0700

    new background and styles

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
-var port = 333;
+var port = 3333;
 app.set('view engine', 'html');
 app.use(express.static('public'));
+app.listen(port, function () {
+    console.log('Server listening on localhost:%s', port);
+});
+app.get('/', function (req, res) {
+    res.sendFile('./index.html');
+})
